dfcacf5c3685924c59cabe838b2d2c33d19cb33d
salveta <salveta4_@hotmail.com>
2014-07-28 19:27:20 +0200

    Add image in the post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/avatar_defecto.jpg"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  
   self.per_page = 10
 
   def timestamp
