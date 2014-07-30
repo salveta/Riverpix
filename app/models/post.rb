666b664c9821dcebdbd884ace8a2bd75b40538f1
salveta <salveta4_@hotmail.com>
2014-07-30 19:13:38 +0200

    Css implemented

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/avatar_defecto.jpg"
+  has_attached_file :avatar, :styles => { :medium => "400x400>", :thumb => "100x100>" }, :default_url => "/assets/avatar_defecto.jpg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-  
+  validates_attachment_size :avatar, :less_than => 1.megabyte
   self.per_page = 10
 
   def timestamp
