7bf138ec2c8354bb90b274b2a71bba0afecef27f
salveta <salveta4_@hotmail.com>
2014-08-14 13:03:00 +0200

    comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
-    @comment.user = current_user
+    @comment.user_id = current_user.id
     @comment.save!
     redirect_to post_path(@post)
   end
@@ -16,6 +16,6 @@ class CommentsController < ApplicationController
  
 private
     def comment_params
-      params.require(:comment).permit(:commenter, :commentbody)
+      params.require(:comment).permit(:commenter, :commentbody, :user_id)
     end
 end
