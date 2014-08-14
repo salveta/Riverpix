0065e3040e7f24f2196bf0f0f2de73898b18357c
salveta <salveta4_@hotmail.com>
2014-08-14 18:19:14 +0200

    s3 amazon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.app_domain = 'http://radiant-headland-4230.herokuapp.com/'
+  config.app_domain = 'http://secret-caverns-5066.herokuapp.com/'
   config.action_mailer.default_url_options = {host: config.app_domain, port: 3000}
   #config send email
   config.action_mailer.delivery_method = :smtp
